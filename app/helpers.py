007c78c9fc6ce396e0cfc74d4d75a573ecfa6916
Peter Gates <albeithome@gmail.com>
2022-05-29 16:04:47 -0500

    streamlit support

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,6 @@
 import re
+import pandas as pd
+import sqlalchemy as sq
 
 def clean_title(title: str) -> str:
     '''
@@ -7,3 +9,25 @@ def clean_title(title: str) -> str:
     '''
     return re.sub("[^a-zA-Z0-9 ]", "", title)
 
+
+def save_data(dataset: pd.DataFrame, table_name: str, if_exists: str = 'fail', index: bool = False) -> bool:
+    '''
+    Saves dataframe to the movie_data sqlite database
+    '''
+    engine = sq.create_engine("sqlite:///data/movie_data.sql")
+    try:
+        with engine.connect() as cnx:
+            dataset.to_sql(name=table_name, con=cnx, if_exists=if_exists, index=index)
+            return True
+    except:
+        print("Does this table already exist in movie_data.sql?")
+        return False
+
+
+def load_data(table_name: str) -> pd.DataFrame:
+    '''
+    Loads table from the movie_data sqlite database
+    '''
+    engine = sq.create_engine("sqlite:///data/movie_data.sql")
+    with engine.connect() as cnx:
+        return pd.read_sql(table_name, cnx)
--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import pandas as pd
 import streamlit as st
-import app.helpers as h
-import app.engine as e
+from app import helpers as h
+from app import engine as e
+
 
 st.title("Movie Recommendations")
-
+@st.cache
+def load_ratings_dataset() -> pd.DataFrame:
+    '''
+    Created solely so that the dataset isn't loaded each time on
+    page refresh
+    '''
+    return pd.read_csv("data/ratings.csv")
 
 def app():
-    movies = pd.read_csv("data/movies.csv")
-    ratings = pd.read_csv("data/ratings.csv")
+    movies = h.load_data('movies')
+    ratings = load_ratings_dataset()
     movies['cleaned_title'] = movies['title'].apply(h.clean_title)
     
-    search_term = st.text_input()
+    search_term = st.text_input("Search for a movie", value='toy story').title()
+
+    if not search_term:
+        st.info("Search for a movie title to get recommendations based on what similar users liked!")
+        st.stop()
+    
+    movie_id = e.search(search_term, movies).iloc[0,0]
+    results = e.find_similar_movies(movies, ratings, movie_id)
+    st.success(f"Recommendations for you that are similar to {search_term}")
+    st.table(results)
+
+if __name__ == "__main__":
+    app()
